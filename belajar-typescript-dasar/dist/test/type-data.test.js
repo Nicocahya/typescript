dfb44f740a8a66d380ac9ff41a3538855d725c13
Nicocahya <nr016082@gmail.com>
2024-03-19 14:33:53 +0700

    type data primitif

---
+++ belajar-typescript-dasar/dist/test/type-data.test.js
@@ -0,0 +1,11 @@
+"use strict";
+describe('Data type', function () {
+    it('should must declare', function () {
+        const name = "Nico cahya";
+        const balance = 999999;
+        const isVip = true;
+        console.info(name);
+        console.info(balance);
+        console.info(isVip);
+    });
+});
---
+++ belajar-typescript-dasar/test/type-data.test.ts
@@ -0,0 +1,12 @@
+describe('Data type', function () {
+    it('should must declare', function () {
+
+        const name: string = "Nico cahya";
+        const balance: number = 999999;
+        const isVip: boolean = true; 
+
+        console.info(name);
+        console.info(balance);
+        console.info(isVip);
+    });
+});
