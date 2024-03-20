e3210f3f9f7a61a0192c3aae3e0bba371f9e2c76
Nicocahya <nr016082@gmail.com>
2024-03-20 14:55:02 +0700

    function parameter

--- belajar-typescript-dasar/test/function.test.ts
+++ belajar-typescript-dasar/test/function.test.ts
@@ -13,4 +13,39 @@ describe('Function', function () {
 
         printHello("Nico")
     })
+
+    it('should support default value', function () {
+        function sayHello(name: string = "Guest"): string {
+            return `Hello ${name}`;
+        }
+
+        expect(sayHello()).toBe("Hello Guest");
+        expect(sayHello("Nico")).toBe("Hello Nico");
+    })
+
+    it('should support rest parameter', function () {
+        function sum(...values: number []): number {
+            let total = 0;
+            for (const value of values) {
+                total += value;
+            }
+            return total;
+        }
+
+        expect(sum(1,2,3,4,5)).toBe(15);
+    })
+
+    it('should support optional parameter', function () {
+        function sayHello(fristName : string, lastName? : string ): string {
+
+            if(lastName){
+            return `Hello ${fristName} ${lastName}`;
+            }else {
+            return `Hello ${fristName}`;
+            }
+        }
+
+        expect(sayHello("Nico")).toBe("Hello Nico");
+        expect(sayHello("Nico", "Cahya")).toBe("Hello Nico Cahya")
+    })
 })
